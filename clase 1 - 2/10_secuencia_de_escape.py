5d953f74902bd1e7f85a2089db257d0fab59bc71
Xaca Rana <xacarana@gmail.com>
2024-11-22 20:19:43 +0000

    Ejemplo ejercicio secuencia de escape

---
+++ clase 1 - 2/10_secuencia_de_escape.py
@@ -0,0 +1,10 @@
+# Imprimir un banner ascii en consola
+# Generar el texto en https://patorjk.com/software/taag/#p=display&f=Fire%20Font-s&t=upb
+
+print("___  ___  ________  ________      ")
+print("|\\  \\|\\  \\|\\   __  \\|\\   __  \\    ")
+print("\\ \\  \\\\\\  \\ \\  \\|\\  \\ \\  \\|\\ /_   ")
+print(" \\ \\  \\\\\\  \\ \\   ____\\ \\   __  \\  ")
+print("  \\ \\  \\\\\\  \\ \\  \\___|\\ \\  \\|\\  \\ ")
+print("   \\ \\_______\\ \\__\\    \\ \\_______\\")
+print("    \\|_______|\\|__|     \\|_______|")
