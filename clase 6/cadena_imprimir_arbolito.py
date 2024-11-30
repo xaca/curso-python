cc08b598ff51844ffe06362cd52056df6de14da6
Andres Bedoya <and1res@hotmail.com>
2024-11-30 11:35:37 -0500

    Actualizacion ejercicio arbol de navidad

--- clase 6/cadena_imprimir_arbolito.py
+++ clase 6/cadena_imprimir_arbolito.py
@@ -3,12 +3,26 @@
 # Ejemplo:
 # n = 5
 #
+#     *     1
+#    ***    3
+#   *****   5
+#  *******  7
+# ********* 9
 #     *
-#    ***
-#   *****
-#  *******
-# *********
-#     *
+
+n = 5
+for i in range(n):
+    #print(i,n,(n-i-1),(2*i+1))
+    print(" "*(n-i-1) + "*"*(2*i+1))
+print(" "*(n-1) + "*")
+
+print()
+n = 20
+salida = ""
+for i in range(n):
+    salida += ("*"*(2*i+1)).center(n*2-1) + "\n"
+salida += "*".center(n*2-1)
+print(salida)
 
 # Imprimir el arbol, pero con un fondo de un caracter
 # 0000*0000
@@ -17,3 +31,10 @@
 # 0*******0
 # *********
 # 0000*0000
+print()
+n = 10
+fondo = "-"
+relleno = "1"
+for i in range(n):
+    print(fondo*(n-i-1) + relleno*(2*i+1) + fondo*(n-i-1))
+print(fondo*(n-1) + relleno + fondo*(n-1))
