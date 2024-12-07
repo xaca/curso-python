dee0da569263bf3a6efde4e537f66e8d2f0df672
Andres Bedoya <and1res@hotmail.com>
2024-12-07 08:13:20 -0500

    Ejercio propuesto de listas, de calentamiento

---
+++ clase 8/01_promedio_listas.py
@@ -0,0 +1,10 @@
+""" 
+Ejercicios:
+ 
+1. Leer una lista de números e imprimir la lista con el siguiente formato de salida:
+
+    |2 - 4 - 6 - 8 - 10|
+
+2. Realizar un programa que pida n valores númericos, y calcule el promedio de los valores ingresados.
+
+"""
