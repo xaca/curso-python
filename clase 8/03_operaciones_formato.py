85a62ed2903b99e4e8480614b3c65f812f62d933
Andres Bedoya <and1res@hotmail.com>
2024-12-07 10:43:14 -0500

    Creación de archivo de ejemplo para la lectura de datos

--- clase 8/03_operaciones_formato.py
+++ clase 8/03_operaciones_formato.py
@@ -33,7 +33,7 @@ print(f"El año es {a}, el mes es {m} y el día es {d}")
         Queso x 12 a $3000  ... $36000    
 """
 
-registro = "2,2-3,3-12,6-34,12;2300#530#1200#3000;Huevo|Leche|Pan|Queso"
+registro = "2,2-3,3-12,6-34,12;2300#530#1200#3000;Torta de carne|Leche|Pan|Queso"
 print(registro)
 datos = registro.split(";")
 codigos = datos[0]
@@ -44,18 +44,50 @@ codigos = codigos.split("-")
 precios = precios.split("#")
 nombres = nombres.split("|")
 
-cantidad = []
+cantidades = []
 cod_temp = []
 temp = ""
 
 for i in range(len(codigos)):
     temp = codigos[i].split(",")
     cod_temp.append(temp[0])
-    cantidad.append(temp[1])
+    cantidades.append(temp[1])
 
 codigos = cod_temp
 
 print(codigos)
 print(precios)
 print(nombres)
-print(cantidad)
+print(cantidades)
+
+#str = "texto largo muy largo"
+#print(str[0:10]) # Recortar un pedazo de la cadena
+total = 0
+
+for i in range(len(codigos)):
+    #Huevo x 2  a $2300  ... $4600
+    nombre = nombres[i]
+    nombre = nombre[0:10]
+    nombre = nombre.ljust(10," ")
+
+    print(f"{nombre} x",end=" ")
+
+    cantidad = cantidades[i]
+    if len(cantidad)<2:
+        cantidad = cantidad.rjust(2,"0")
+    
+    print(f"{cantidad}",end=" ")
+
+    precio = precios[i].rjust(7," ")
+    print(f"a ${precio}",end=" ")
+
+    subtotal = int(cantidad)*int(precios[i])
+    total += subtotal
+    print(f"... ${subtotal}")
+
+total = str(total)
+print(f"Total: ${total.rjust(29,' ')}")
+
+    # TODO: En la salida de impresion, pegar el caracter de dinero, del valor del precio del producto. Es decir que quede $2300 en lugar de $    2300
+
+    # TODO: Ajustar la palabra total para que se alinee con el valor total de la compra.
