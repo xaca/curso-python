9b4c7bf6e5b667b203502981f81659166b5aae28
Andres Bedoya <and1res@hotmail.com>
2024-12-13 18:59:57 -0500

    Actualizacion de lectura de datos

--- clase 9/02_ejercicio_salarios.py
+++ clase 9/02_ejercicio_salarios.py
@@ -1,3 +1,57 @@
 """
+ 1. Leer el salario de todos los empleados de una empresa, y guardarlos en una lista. La entrada de datos finaliza cuando el usuario escriba el número cero (0) en el salario. Al finalizar se deben mostrar los siguientes resultados:
 
+    - Promedio del salario de todos los empleados.
+    - Salario más alto y salario más bajo.
+    - Número de empleados con salarios inferiores a $10.000.000.
+    - Promedio del salario de los empleados que ganan más de $10.000.000.
 """
+
+def leer_salarios():
+    # Lectura de salarios
+    salarios = []
+    salario = 0
+    while True:
+        try:
+            salario = float(input("Ingrese el salario del empleado: "))
+            if salario != 0:
+                if salario > 0:
+                    if salario >= 1300000 and salario <= 15000000:
+                        salarios.append(salario)
+                    else:
+                        print("El salario debe estar entre $1.300.000 y $15.000.000")
+                else:
+                    print("Ingrese un salario válido")
+            else:
+                return salarios
+        except:
+            print("Ingrese un salario válido")
+
+def promedio_salarios(lista_salarios):
+    # Promedio de salarios
+    print("Promedio de salarios")
+
+def salario_mayor(lista_salarios):
+    # Salario mayor
+    print("Salario mayor")
+
+def salario_menor(lista_salarios):
+    # Salario menor
+    print("Salario menor")
+
+def empleados_salario_inferior(lista_salarios):
+    # Empleados con salario inferior a 10.000.000
+    print("Empleados con salario inferior a 10.000.000")
+
+def promedio_salario_mayor_10m(lista_salarios):
+    # Promedio de salario de empleados con salario mayor a 10.000.000
+    print("Promedio de salario de empleados con salario mayor a 10.000.000")
+
+def main():
+    lista_salarios = leer_salarios()
+    print(lista_salarios)
+    #lista_salarios = leer_salarios()
+    #promedio_salarios(lista_salarios)
+
+if __name__ == "__main__":
+    main()
