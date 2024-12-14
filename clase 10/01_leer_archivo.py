e7283626d6bc671d0eed5881bda9f7e575c9733c
Andres Bedoya <and1res@hotmail.com>
2024-12-14 09:30:57 -0500

    teoria de archivos

--- clase 10/01_leer_archivo.py
+++ clase 10/01_leer_archivo.py
@@ -1,3 +1,13 @@
-resultado = open("archivo.txt", "r")
+import os
+
+#ruta = os.path.abspath("archivo.txt") # No se obtiene la ruta correcta, se obtiene la ruta del archivo que se está ejecutando
+#ruta = os.getcwd() # Se obtiene la ruta del archivo que se está ejecutando
+
+#print(__file__)
+#print(os.path.dirname(__file__))
+#print(os.path.join(os.path.dirname(__file__), "archivo.txt"))
+
+ruta = os.path.join(os.path.dirname(__file__), "archivo.txt")
+resultado = open(ruta, "r", encoding="utf-8")
 print(resultado.read())
 resultado.close()
